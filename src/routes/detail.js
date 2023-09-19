a395d4ef92120117155b6d5850ab5cfc5c444e70
HK <yhu930421@naver.com>
2023-09-19 17:23:02 +0900

    life cycle

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import {useState, useEffect} from "react";
 
 let BtnStyled = styled.button`
   background: ${ props=> props.bg };
@@ -10,8 +11,30 @@ let BlackBox = styled.div`
   background:grey;
   padding:20px;
 `
+let MsgBox = styled.div`
+  width:100%;
+  height:50px;
+  color:#000;
+  text-align:center;
+  line-height:50px;
+  background:grey;
+  border:1px solid #000;
+  border-radius:10px;
+  margin-top:20px;
+`
 
 function Detail(props){
+
+  useEffect(()=>{
+    for (var i = 0; i < 10000; i++){
+      console.log(i);
+    }
+  })
+
+  setTimeout( ()=> {},2000);
+  
+
+  let [count,setCount] = useState(0);
 
   let {id} = useParams();
   let findPrd = props.shoes.find(function(x){
@@ -20,11 +43,14 @@ function Detail(props){
 
   return (
     <div className="container">
+      {count}
+      <button onClick={()=>{ setCount(count+1) }}>Button</button>
       <BlackBox>
           <BtnStyled bg="yellow">Button</BtnStyled>
           <BtnStyled bg="blue">Button</BtnStyled>
           <BtnStyled bg="orange">Button</BtnStyled>
       </BlackBox>
+      <MsgBox>2초이내 구매 시 할인</MsgBox>
       <div className="row">
         <div className="col-md-6">
           <img src={`https://codingapple1.github.io/shop/shoes${id}.jpg`} width="100%" alt="detail img" />
